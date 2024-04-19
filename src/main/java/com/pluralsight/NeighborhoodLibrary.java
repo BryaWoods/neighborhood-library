f3519e5ccce091ac97573e7aeb217f0b39bd4612
BryaWoods <bryannewoods@gmail.com>
2024-04-19 16:51:01 -0400

    updates to library

--- src/main/java/com/pluralsight/NeighborhoodLibrary.java
+++ src/main/java/com/pluralsight/NeighborhoodLibrary.java
@@ -3,5 +3,20 @@ package com.pluralsight;
 public class NeighborhoodLibrary {
     public static void main(String[] args) {
 
+        Book[] books = new Book[5];
+
+        books[0] = new Book(666, "9783833936326", "Diary of a Wimpy Kid");
+        books[1] = new Book(777, "9780385335485", "Confessions of a Shopaholic");
+        books[2] = new Book(999,"978-0062094360", "The Exorcist");
+        books[3] = new Book(420, "10: 0688171583", "Tha Doggfather: The Times, Trials, And Hardcore Truths Of Snoop Dogg");
+        books[4] = new Book(888, "1846460441", "Bratz: Pop Princesses");
+
+
+
+
+
+
+
+
     }
 }
